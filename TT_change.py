b1f75a9719428568ab5fbb6d32b3adbdb50c2af7
Youwei Zhu <1807279653@qq.com>
2017-02-15 12:34:58 +0800

    add_TT change

--- TT_change.py
+++ TT_change.py
@@ -1,3 +1,4 @@
+import sys
 a="./keras/keras.json"
 def to_theano():
 	old=open(a).read()
@@ -8,4 +9,13 @@ def to_tensorflow():
 	old=open(a).read()
 	new=old.replace("theano","tensorflow")
 	open(a,"w").write(new)
-	
+
+if __name__ == '__main__':
+	print(sys.argv)
+	print(sys.argv[1])
+	if sys.argv[1]=="tensorflow":
+		to_tensorflow()
+	elif sys.argv[1]=="theano":
+		to_theano()
+	else:
+		print ("Unknown command, try python TT_change.py tensorflow   or python TT_change.py theano")
